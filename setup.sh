1e9ecc2c52bfcec06a8d3ff26bb2aac648d8353c
connorfoody <connorfoody>
2015-10-04 13:09:51 -0400

    updated top repo

--- setup.sh
+++ setup.sh
@@ -4,4 +4,14 @@ mkdir src
 mkdir bin
 mkdir pkg
 
-DIR=$(cd "$( dirname "
+# get all the repos for this project
+go get github.com/opinionated/scraper-core
+go get github.com/opinionated/scheduler
+go get github.com/opinionated/utils
+
+# get all the dependancies
+go get github.com/golang/mock/mockgen
+go get github.com/golang/mock/gomock
+
+go get golang.org/x/net/html
+
