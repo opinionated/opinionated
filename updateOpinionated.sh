3c81607ee3e1578c413223982999a8b4925c5d1b
connorfoody <connorfoody>
2015-10-07 11:06:36 -0400

    added update script

---
+++ updateOpinionated.sh
@@ -0,0 +1,9 @@
+# script to update all of the packages in opinionated
+# called using go get
+
+
+go get -u -f ...config
+go get -u -f ...scraper-core
+go get -u -f ...scraper
+go get -u -f ...scheduler
+go get -u -f ...fetcher
