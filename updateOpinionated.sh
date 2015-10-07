41037d109953d7709ee86fb876d39f75cd910ccc
connorfoody <connorfoody>
2015-10-07 14:28:48 -0400

    updated alchemy to use net requests instead of curl

--- updateOpinionated.sh
+++ updateOpinionated.sh
@@ -1,9 +1,11 @@
 # script to update all of the packages in opinionated
 # called using go get
 
-
 go get -u -f ...config
+go get -u -f ...scheduler
+
 go get -u -f ...scraper-core
 go get -u -f ...scraper
-go get -u -f ...scheduler
 go get -u -f ...fetcher
+
+go get -u -f ...analyzer-core
