335575675edce8377a59bb1d374cf9fc1afad884
ConnorFoody <connorfoody@gmail.com>
2016-03-05 14:45:12 -0500

    added artcle store

--- updateOpinionated.sh
+++ updateOpinionated.sh
@@ -2,30 +2,28 @@
 
 # updates all the opinionated source code
 # go get shouldn't overwrite a non-master branch,
-# but I haven't tested that. This does not get
+# but I haven't tested thatgithub.com/opinionatedhis does not get
 # non-go files so you may want to do a git pull
 # in the specific folder 
 
 
 # from utils
-go get .../config
-go get .../log
+go get github.com/opinionated/util
 
 # from scraper-core
-go get .../scraper-core/scraper
-go get .../scraper-core/net
-go get .../pipeline
+go get github.com/opinionated/scraper-core
 
 # from analyzer
-go get .../alchemy
-go get .../analyzer-core/analyzer
+go get github.com/opinionated/analyzer-core
 
 # from scheduler
-go get .../scheduler
+go get github.com/opinionated/scheduler
 
 # from scrapeServer
-go get .../scrapeServer
+go get github.com/opinionated/scrapeServer
 
 # from scrapeClient
-go get .../scrapeClient
+go get github.com/opinionated/scrapeClient
 
+# articleStore
+go get github.com/opinionated/articleStore
